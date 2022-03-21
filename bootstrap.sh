18364f92db7000b72adb712d5b1cb441cc4f5cd2
cameo69 <44269764+cameo69@users.noreply.github.com>
2022-03-21 23:43:36 +0100

    update for signald v0.17.0-53-5931020a

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,6 @@ env >> /etc/environment
 
 socat -d -v TCP-LISTEN:12345,reuseaddr,fork UNIX-CLIENT:/signald/signald.sock &
 
-/usr/local/bin/signald -d /signald -s /signald/signald.sock
+#/usr/local/bin/signald -d /signald -s /signald/signald.sock
+#as of signald v0.17.0-53-5931020a run ["/entrypoint.sh"] ["-d", "/signald", "-s", "/signald/signald.sock"]
+/entrypoint.sh -d /signald -s /signald/signald.sock
