e1d157bc8d0fd97a8fa16bccde51ffebd93c3487
cameo69 <44269764+cameo69@users.noreply.github.com>
2022-04-20 12:37:35 +0200

    entrypoint.sh moved to /bin

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,4 @@ socat -d -v TCP-LISTEN:12345,reuseaddr,fork UNIX-CLIENT:/signald/signald.sock &
 
 #/usr/local/bin/signald -d /signald -s /signald/signald.sock
 #as of signald v0.17.0-53-5931020a run ["/entrypoint.sh"] ["-d", "/signald", "-s", "/signald/signald.sock"]
-/entrypoint.sh -d /signald -s /signald/signald.sock
+/bin/entrypoint.sh -d /signald -s /signald/signald.sock
